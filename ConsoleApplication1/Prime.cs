49267e8b97c50dcd47820d90b46ff48f8e00a526
davidnastaseCALIBRE <david.nastase@calibresys.com>
2016-03-10 08:48:29 -0500

    Initial Commit. Contains Unit tests and code for calculating whether or not a number is prime.
    It's probably a terrible implementation, but it uses the Sieve of Erasthenes.

---
+++ ConsoleApplication1/Prime.cs
@@ -0,0 +1,28 @@
+﻿namespace DavidMath
+{
+    public class Prime
+    {
+        public static bool isPrime(int a)
+        {
+            if (a < 2)
+            {
+                return false;
+            }
+            bool[] holder = new bool[a+1]; //default values are false, so we'll be inverting some.
+            for (int b = 2; b < a; b++)
+            {
+                if (!holder[b]) 
+                {
+                    for(int c = b; c <= a; c += b)
+                    {
+                        if(c <= a &&  c != b)
+                        {
+                            holder[c] = true; 
+                        }
+                    }
+                }
+            }
+            return !holder[a];
+        }
+    }
+}
