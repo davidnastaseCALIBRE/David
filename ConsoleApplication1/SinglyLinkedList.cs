38c4bfd04aa8a83cf48444f5fa15a8adfd19b7a6
davidnastaseCALIBRE <david.nastase@calibresys.com>
2016-03-10 16:30:16 -0500

    Unordered Singly linked list implementation. also untested.

---
+++ ConsoleApplication1/SinglyLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+namespace DavidMath {
+    public class SinglyLinkedList {
+        private node head;
+
+        public SinglyLinkedList() { }
+
+        public void add(int a){
+            if (head != null)
+                head.add(a);
+            else
+                head = new node(a, null);
+        }
+
+        public List<int> reasonableList(){
+            if(head != null)
+                return head.reasonableList();
+            return new List<int>();
+        }
+    }
+
+    class node{
+        private int value;
+        private node tail;
+
+        public node(int a, node b){
+            value = a;
+            tail = b;
+        }
+
+        public void add(int a){
+            if (tail == null)
+                tail = new node(a, null);
+            tail.add(a);
+        }
+
+        public List<int> reasonableList(){
+            List<int> holder = new List<int>();
+            holder.Add(value);
+            if (tail != null)
+                holder.AddRange(tail.reasonableList());
+            return holder;
+        }
+    }
+}
