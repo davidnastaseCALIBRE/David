51f6de11bfdecfde8d1c13c76a493642615bcb9a
davidnastaseCALIBRE <david.nastase@calibresys.com>
2016-03-11 11:16:01 -0500

    Added Unit tests for Binary Search Tree and fixed all missed bugs with it that they uncovered.

--- ConsoleApplication1/BinarySearchTree.cs
+++ ConsoleApplication1/BinarySearchTree.cs
@@ -2,58 +2,144 @@
 using System.Text;
 
 namespace DavidMath{
-    public class BinarySearchTree{
-        private int value;
-        private BinarySearchTree leftNode;
-        private BinarySearchTree rightNode;
-        public BinarySearchTree(int a){
-            value = a;
+    public class BinarySearchTree {
+        private Leaf head;
+
+        public BinarySearchTree(){ }
+
+        public void Add(int a){
+            if (head == null)
+                head = new Leaf(a);
+            else
+                head.Add(a);
+        }
+
+        public void Remove(int a){
+            if (Contains(a)){
+                if (head.Childless())
+                    head = null;
+                else{
+                    head.Remove(a);
+                }
+            }
         }
 
         public bool Contains(int a){
-            if (a > value && rightNode != null)
-                return rightNode.Contains(a);
-            if (a < value && leftNode != null)
-                return leftNode.Contains(a);
-            return value == a;
+            if (head != null)
+                return head.Contains(a);
+            return false;
         }
 
-        public void Add(int a){ //Does nothing if the BST already contains that value.
-            if (a > value){
-                if (rightNode == null)
-                    rightNode = new BinarySearchTree(a);
-                else
-                    rightNode.Add(a);
-            }
-            if (a < value){
-                if (rightNode == null)
-                    rightNode = new BinarySearchTree(a);
-                else
-                    rightNode.Add(a);
-            }
-        }
-
-        //Yussss recursion
-        public List<int> treeOrderedList(){
-            List<int> holder = new List<int>();
-            if (leftNode != null)
-                holder.AddRange(leftNode.treeOrderedList());
-            holder.Add(value);
-            if(rightNode != null){
-                holder.AddRange(rightNode.treeOrderedList());
-            }
-            return holder;
+        public List<int> OrderedList(){
+            if (head != null)
+                return head.TreeOrderedList();
+            return new List<int>();
         }
 
         public override string ToString(){
-            StringBuilder x = new StringBuilder();
-            if (leftNode != null)
-                x.Append(leftNode.ToString());
-            x.Append(value);
-            if (rightNode != null)
-                x.Append(rightNode.ToString());
-     
-            return x.ToString();
+            if (head != null)
+                return head.ToString();
+            return "";
+        }
+
+        private class Leaf {
+            private int value;
+            private Leaf leftLeaf;
+            private Leaf rightLeaf;
+            public Leaf(int a) {
+                value = a;
+            }
+
+            public bool Contains(int a) {
+                if (a > value && rightLeaf != null)
+                    return rightLeaf.Contains(a);
+                if (a < value && leftLeaf != null)
+                    return leftLeaf.Contains(a);
+                return value == a;
+            }
+
+            public void Add(int a) { //Does nothing if the BST already contains that value.
+                if (a > value) {
+                    if (rightLeaf == null)
+                        rightLeaf = new Leaf(a);
+                    else
+                        rightLeaf.Add(a);
+                }
+                if (a < value) {
+                    if (leftLeaf == null)
+                        leftLeaf = new Leaf(a);
+                    else
+                        leftLeaf.Add(a);
+                }
+            }
+
+            public void Remove(int a){
+                if (Contains(a)){
+                    if(a > value){
+                        if (!rightLeaf.Childless())
+                            rightLeaf.Remove(a);
+                        else
+                            rightLeaf = null;
+                    }
+                    else if( a < value){
+                        if(!leftLeaf.Childless())
+                            leftLeaf.Remove(a);
+                        else
+                            leftLeaf = null;
+                    }
+                    else{
+                        if(LeftSuccessor() != value){
+                            int temp = LeftSuccessor();
+                            Remove(temp);
+                            value = temp;
+                        }else{
+                            int temp = RightSuccessor();
+                            Remove(temp);
+                            value = temp;
+                        }
+                    }
+                }
+            }
+
+            private int LeftSuccessor()
+            {
+                if (rightLeaf != null)
+                    return rightLeaf.LeftSuccessor();
+                return value;
+            }
+
+            private int RightSuccessor()
+            {
+                if (leftLeaf != null)
+                    return leftLeaf.LeftSuccessor();
+                return value;
+            }
+
+            public List<int> TreeOrderedList() {
+                List<int> holder = new List<int>();
+                if (leftLeaf != null)
+                    holder.AddRange(leftLeaf.TreeOrderedList());
+                holder.Add(value);
+                if (rightLeaf != null) {
+                    holder.AddRange(rightLeaf.TreeOrderedList());
+                }
+                return holder;
+            }
+
+            public override string ToString() {
+                StringBuilder x = new StringBuilder();
+                if (leftLeaf != null)
+                    x.Append(leftLeaf.ToString());
+                x.Append(value);
+                if (rightLeaf != null)
+                    x.Append(rightLeaf.ToString());
+
+                return x.ToString();
+            }
+
+            public bool Childless(){
+                return leftLeaf == null && rightLeaf == null;
+            }
         }
     }
 }
---
+++ UnitTestProject1/BinarySearchTreeTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using DavidMath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BinarySearchTreeTest
+    {
+        [TestMethod]
+        public void ToStringTest()
+        {
+            BinarySearchTree tester = new BinarySearchTree();
+            Assert.AreEqual("", tester.ToString());
+
+            tester.Add(4);
+            Assert.AreEqual("4", tester.ToString());
+
+            tester.Add(2);
+            Assert.AreEqual("24", tester.ToString());
+
+            tester.Add(6);
+            Assert.AreEqual("246", tester.ToString());
+        }
+
+        [TestMethod]
+        public void OrderedListTest()
+        {
+            BinarySearchTree tester = new BinarySearchTree();
+            Assert.IsTrue(tester.OrderedList().Count == 0);
+
+            tester.Add(4);
+            Assert.AreEqual(1, tester.OrderedList().Count);
+            Assert.IsTrue(tester.OrderedList().Contains(4));
+
+            tester.Add(2);
+            Assert.IsTrue(tester.OrderedList().Contains(2));
+            Assert.AreEqual(0, tester.OrderedList().IndexOf(2));
+
+            tester.Add(6);
+            Assert.IsTrue(tester.OrderedList().Contains(6));
+            Assert.AreEqual(2, tester.OrderedList().IndexOf(6));
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            BinarySearchTree tester = new BinarySearchTree();
+            Assert.IsFalse(tester.Contains(0));
+            Assert.IsFalse(tester.Contains(1));
+
+            tester.Add(5);
+            Assert.IsTrue(tester.Contains(5));
+
+            tester.Add(7); //Check right child
+            Assert.IsTrue(tester.Contains(7));
+
+            tester.Add(6); //Check right child left subchild
+            Assert.IsTrue(tester.Contains(6));
+
+            tester.Add(9);
+            Assert.IsTrue(tester.Contains(9)); //check right recursive child
+
+            tester.Add(3);
+            Assert.IsTrue(tester.Contains(3)); //check left child
+
+            tester.Add(4); //Check left child right subchild
+            Assert.IsTrue(tester.Contains(4));
+
+            tester.Add(1);
+            Assert.IsTrue(tester.Contains(1)); //check left recursive child
+
+            Assert.IsFalse(tester.Contains(18)); //checks that children report false correctly.
+        }
+
+        [TestMethod]
+        public void AddTest() // note, add has been extensively tested by the other tests so far.
+        {
+            BinarySearchTree tester = new BinarySearchTree();
+            tester.Add(4);
+            Assert.AreEqual("4", tester.ToString());
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            BinarySearchTree tester = new BinarySearchTree();
+            tester.Remove(4); //let's remove from null. Let's hope for no exceptions
+            Assert.IsFalse(tester.Contains(4));
+
+            tester.Add(4);
+            tester.Add(2);
+            tester.Add(3);
+            tester.Add(1);
+
+            tester.Remove(2);
+            Assert.IsFalse(tester.Contains(2)); //remove child node with two children.
+            Assert.AreEqual(3, tester.OrderedList().Count);
+
+            tester.Remove(3); //remove child with 1 child.
+            Assert.IsFalse(tester.Contains(3)); //remove child node with one child.
+            Assert.AreEqual(2, tester.OrderedList().Count);
+
+            tester.Remove(4);
+            Assert.IsFalse(tester.Contains(4)); // remove head node with one child.
+            Assert.AreEqual(1, tester.OrderedList().Count);
+
+            tester.Remove(1);
+            Assert.IsFalse(tester.Contains(1)); // remove childless head node.
+            Assert.AreEqual(0, tester.OrderedList().Count);
+
+        }
+    }
+}
